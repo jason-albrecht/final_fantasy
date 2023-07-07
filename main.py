5838b195462d5eebc303435e01459a18abf7dbd2
Jason Albrecht <albrecht.jason@gmail.com>
2023-07-06 21:44:36 -0400

    add battle start messages

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from objects.Arena import Arena
+from objects.Battle import Battle
+from objects.Character import Character
 
 entities: list = []
 
 def main():
-    arena = Arena(entities)
+    # #TODO create a character
+    player = Character('Jason', 100, 20, 1)
+    entities.append(player)
+    # #TODO test that default alignment works
+    # #TODO test that eplicit alignment works
+
+    # #TODO create an enemy
+    enemy = Character()
+    entities.append(enemy)
+
+    #TODO start a battle with character vs. enemy
+    battle = Battle(entities)
 
 
 if __name__ == '__main__':
