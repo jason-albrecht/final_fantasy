6e63fad5f99154e1f0f0761784e9e300b1caaae6
Jason Albrecht <albrecht.jason@gmail.com>
2023-07-06 21:58:12 -0400

    re-add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from objects.Battle import Battle
+from objects.Character import Character
+from objects.Alignment import Alignment
+
+entities: list = []
+
+def main():
+    # #TODO create a character
+    player = Character('Jason', 100, 20, 1)
+    entities.append(player)
+    # #TODO test that default alignment works
+    # #TODO test that eplicit alignment works
+
+    # #TODO create an enemy
+    enemy = Character('Demon', 20, 5, alignment=2)
+    entities.append(enemy)
+
+    #TODO start a battle with character vs. enemy
+    battle = Battle()
+
+
+if __name__ == '__main__':
+    main()
