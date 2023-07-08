f8eeb2309bb4b343d62ec832d423b46c4f167733
Jason Albrecht <albrecht.jason@gmail.com>
2023-07-08 07:22:03 -0400

    remove Arena, default_factory from Battle

--- objects/Battle.py
+++ objects/Battle.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 class Battle():
     '''Start a battle'''
 
-    _entities: list = field(default_factory=True)
+    _entities: list
     _team_count: int = field(default = 2, repr=False)
     _obj_type: str = field(default = 'battle', repr=False)
     _start_message: str = field(default='Battle started!', repr=False)
